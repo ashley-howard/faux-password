978381167cdcd4a4c740ad10da43a01c647330bb
Ashley Howard <me@ashley.how>
2020-12-18 12:21:56 +0000

    add words

--- main.js
+++ main.js
@@ -13,7 +13,19 @@ function passwordSubmit() {
     }
 }
 
-function resetForm(){
+function resetForm() {
     localStorage.clear()
-
+    document.getElementById('reset-text').style.display = 'flex'
 }
+
+
+// Execute a function when the user releases a key on the keyboard
+document.getElementById('password').addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("password-btn").click();
+    }
+  });
